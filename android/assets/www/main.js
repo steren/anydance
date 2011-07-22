d4788d564abd53d7a5949784ddacd5d6de83d0b4
Steren <steren.giannini@gmail.com>
2011-07-22 11:21:24 +0200

    only keep what is needed for the app: accelerometer, vibration and network

--- android/assets/www/main.js
+++ android/assets/www/main.js
@@ -5,20 +5,6 @@ var deviceInfo = function() {
     document.getElementById("name").innerHTML = device.name;
     document.getElementById("width").innerHTML = screen.width;
     document.getElementById("height").innerHTML = screen.height;
-    document.getElementById("colorDepth").innerHTML = screen.colorDepth;
-};
-
-var getLocation = function() {
-    var suc = function(p) {
-        alert(p.coords.latitude + " " + p.coords.longitude);
-    };
-    var locFail = function() {
-    };
-    navigator.geolocation.getCurrentPosition(suc, locFail);
-};
-
-var beep = function() {
-    navigator.notification.beep(2);
 };
 
 var vibrate = function() {
@@ -37,6 +23,7 @@ function updateAcceleration(a) {
     document.getElementById('x').innerHTML = roundNumber(a.x);
     document.getElementById('y').innerHTML = roundNumber(a.y);
     document.getElementById('z').innerHTML = roundNumber(a.z);
+    document.getElementById('timestamp').innerHTML = a.timestamp;
 }
 
 var toggleAccel = function() {
@@ -50,7 +37,7 @@ var toggleAccel = function() {
         accelerationWatch = null;
     } else {
         var options = {};
-        options.frequency = 1000;
+        options.frequency = 100;
         accelerationWatch = navigator.accelerometer.watchAcceleration(
                 updateAcceleration, function(ex) {
                     alert("accel fail (" + ex.name + ": " + ex.message + ")");
@@ -58,61 +45,8 @@ var toggleAccel = function() {
     }
 };
 
-var preventBehavior = function(e) {
-    e.preventDefault();
-};
-
-function dump_pic(data) {
-    var viewport = document.getElementById('viewport');
-    console.log(data);
-    viewport.style.display = "";
-    viewport.style.position = "absolute";
-    viewport.style.top = "10px";
-    viewport.style.left = "10px";
-    document.getElementById("test_img").src = "data:image/jpeg;base64," + data;
-}
-
 function fail(msg) {
     alert(msg);
-}
-
-function show_pic() {
-    navigator.camera.getPicture(dump_pic, fail, {
-        quality : 50
-    });
-}
-
-function close() {
-    var viewport = document.getElementById('viewport');
-    viewport.style.position = "relative";
-    viewport.style.display = "none";
-}
-
-// This is just to do this.
-function readFile() {
-    navigator.file.read('/sdcard/phonegap.txt', fail, fail);
-}
-
-function writeFile() {
-    navigator.file.write('foo.txt', "This is a test of writing to a file",
-            fail, fail);
-}
-
-function contacts_success(contacts) {
-    alert(contacts.length
-            + ' contacts returned.'
-            + (contacts[2] && contacts[2].name ? (' Third contact is ' + contacts[2].name.formatted)
-                    : ''));
-}
-
-function get_contacts() {
-    var obj = new ContactFindOptions();
-    obj.filter = "";
-    obj.multiple = true;
-    obj.limit = 5;
-    navigator.service.contacts.find(
-            [ "displayName", "name" ], contacts_success,
-            fail, obj);
 }
 
 var networkReachableCallback = function(reachability) {
@@ -133,8 +67,5 @@ function check_network() {
 }
 
 function init() {
-    // the next line makes it impossible to see Contacts on the HTC Evo since it
-    // doesn't have a scroll button
-    // document.addEventListener("touchmove", preventBehavior, false);
     document.addEventListener("deviceready", deviceInfo, true);
 }
